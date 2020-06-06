1b90a8c839779f0079cd5ed79b32c3f0d6c497c5
saikiran sangam <saikiran.sangam7@gmail.com>
2020-06-06 23:12:28 +0530

    Data Studio

--- datastudio/settings.py
+++ datastudio/settings.py
@@ -86,8 +86,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
